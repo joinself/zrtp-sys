345b7c80fdd9793a5c5dbb54bc782e1a82e6d37c
Tom Bevan <tom.bevan@r3labs.io>
2021-12-07 13:34:55 +0000

    working

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(deref_nullptr)]
+#![allow(improper_ctypes)]
 
 include!(concat!(env!("OUT_DIR"), "/zrtp.rs"));
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
 
-        
+        unsafe {
+            let zw = zrtp_CreateWrapper();
+            print!("{:?}", zw);
+        }
     }
 }
